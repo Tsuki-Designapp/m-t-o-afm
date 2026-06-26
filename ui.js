4192b0b0ecf1a1724c5c083abebf073efe60a230
Florian Till <flori@Florian-PCp.localdomain>
2026-06-26 11:52:21 +0200

    en bien

---
+++ ui.js
@@ -0,0 +1,21 @@
+function afficherMeteoActuelle(data) {
+    document.getElementById("meteo-actuelle").innerHTML = `
+        <h2>${data.location.name}, ${data.location.country}</h2>
+        <p>Température : ${data.current.temp_c} °C</p>
+        <p>Ressenti : ${data.current.feelslike_c} °C</p>
+        <p>Condition : ${data.current.condition.text}</p>
+        <p>Humidité : ${data.current.humidity} %</p>
+        <p>Vent : ${data.current.wind_kph} km/h</p>
+    `;
+};
+
+function afficherPrevisions(data) {
+    document.getElementById("cartes-prevision").innerHTML = `
+        <h2>${data.location.name}, ${data.location.country}</h2>
+        <p>Température : ${data.current.temp_c} °C</p>
+        <p>Ressenti : ${data.current.feelslike_c} °C</p>
+        <p>Condition : ${data.current.condition.text}</p>
+        <p>Humidité : ${data.current.humidity} %</p>
+        <p>Vent : ${data.current.wind_kph} km/h</p>
+    `;
+};
